182664db17da9c8ee73af2c2eeb6e3a90f4c004a
HanKiPark <bule177@naver.com>
2021-11-30 00:01:32 +0900

    00:01 push

--- mylayout/src/main/java/com/example/mylayout/MainActivity.java
+++ mylayout/src/main/java/com/example/mylayout/MainActivity.java
@@ -8,72 +8,113 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     GridLayout lin;
     static int startNum =1;
-
+    static int checkNum=0;
+    Button rebtn;
+    ArrayList<Integer> fillNum = new ArrayList<Integer>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lin= findViewById(R.id.linear);
+        rebtn = findViewById(R.id.restart);
+
+      //  lin.setOrientation(LinearLayout.VERTICAL);
+
+        List<Integer> num = new ArrayList<Integer>();
+        List<Integer> nextNum = new ArrayList<Integer>();
+        List<Integer> reNum = new ArrayList<Integer>();
+
+        for(int i=1;i<=25;i++){num.add(i);}
+        for(int i=1;i<=25;i++){reNum.add(i);}
+        for(int i=26;i<=50;i++){nextNum.add(i);}
+        Collections.shuffle(num);
+        Collections.shuffle(reNum);
+
+
 
         View.OnClickListener handler=v->{
-            System.out.println(v.getId());
-            if(v.getId()==startNum){
-                ((Button)v).setText("");
-                startNum++;
-            }
-            if(startNum==17){
-                Toast.makeText(this,"게임종료",Toast.LENGTH_LONG).show();
-                startNum=0;
-            }
-            //클릭한 버튼의 숫자값을 비교 startnum이 같다면 버튼의 text 리셋
+          //  System.out.println(Integer.parseInt(((Button)v).getText().toString()));
+            if(((Button)v).getText().toString()!="") {
+                if (Integer.parseInt(((Button) v).getText().toString()) == startNum) {
+                    ((Button) v).setText("");
+                    startNum++;
+                    checkNum++;
+                    System.out.println("선택" + v.getId());
+                    fillNum.add(v.getId());
+                    if (checkNum >= 5 && checkNum < 30) {
+                        Collections.shuffle(fillNum);
+                        Button raBtn = (Button) findViewById(fillNum.get(3));
+                        raBtn.setText("" + nextNum.get(startNum - 6));
 
+                        System.out.println(raBtn.getId());
+//                        System.out.println("시작");
+//                        for (Object object : fillNum) {
+//                            System.out.println(object);
+//                        }
+
+                        fillNum.remove(3);
+
+                    }
+                }
+                if (startNum == 51) {
+                    Toast.makeText(this, "게임종료", Toast.LENGTH_LONG).show();
+                    startNum = 0;
+                }
+                //클릭한 버튼의 숫자값을 비교 startnum이 같다면 버튼의 text 리셋
+            }
         };
-      //  lin.setOrientation(LinearLayout.VERTICAL);
-        int k=0;
-        int a=0;
-        Integer num[] = new Integer[16];
-        Integer[] arr;
 
-        while(k==0){
 
-     for(int i =0;i<16;i++) {
-         while (a == 0) {
-             int ran = (int) Math.floor(Math.random() * 16 + 1);
 
-             if (ran == 17) {
-                 ran--;
-             }
-
-             num[i] = ran;
-             Set<Integer> set = new HashSet<Integer>(Arrays.<Integer>asList(num));
-             arr = set.toArray(new Integer[0]);
-             System.out.println(num[i]);
-             if(arr.length==num.length){a=1;}
-         }
-         a = 0;
-     }
-     if(num.length==16){k=1;}
-
- }
-        for(int i =0;i<16;i++){
+        for(int i =0;i<25;i++){
 
             Button btn = new Button(this);
 
-            btn.setText(""+num[i]);
-            btn.setId(num[i]);
+
+            btn.setText(""+ num.get(i));
+            btn.setId(num.get(i));
+            btn.setTextSize(30);
 
             btn.setOnClickListener(handler);
-            lin.addView(btn);
+            lin.addView(btn,150,150);
 
         }
 
+        rebtn.setOnClickListener(v->{
+            startNum =1;
+            checkNum=0;
+            fillNum.clear();
+
+            Collections.shuffle(reNum);
+            for(int i =0;i<25;i++){
+
+                Button btn = (Button) findViewById(num.get(i));
+               // btn.setText(""+ reNum.get(i));
+                lin.removeView(btn);
+            }
+            for(int i =0;i<25;i++){
+
+                Button btn = new Button(this);
+
+
+                btn.setText(""+ reNum.get(i));
+                btn.setId(reNum.get(i));
+                btn.setTextSize(30);
+
+                btn.setOnClickListener(handler);
+                lin.addView(btn,150,150);
+
+            }
+
+
+        });
 
     }
 }
