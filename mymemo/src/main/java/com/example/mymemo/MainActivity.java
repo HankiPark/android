2c290d0accc8688b0267ffd5baa94db94c7b7c4f
HankiPark <bule177@naver.com>
2021-12-02 17:48:28 +0900

    123

--- mymemo/src/main/java/com/example/mymemo/MainActivity.java
+++ mymemo/src/main/java/com/example/mymemo/MainActivity.java
@@ -8,16 +8,22 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText txtResult;
+    ListView lv;
     Button btnASel,btn1;
 
     @Override
@@ -37,16 +43,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        txtResult = findViewById(R.id.txtResult);
-        btn1=findViewById(R.id.btn1);
-        btnASel = findViewById(R.id.btnASel);
-
         DatabaseHelper dbHelper = new DatabaseHelper(getApplicationContext());
 
+        btn1=findViewById(R.id.btn1);
+        btnASel = findViewById(R.id.btnASel);
+        lv = findViewById(R.id.lv);
+        SQLiteDatabase db=dbHelper.getReadableDatabase();
+
+        List<Map<String,String>> list= new ArrayList<>();
+        Map<String,String> map = new HashMap<String,String>();
+        map.put("name","홍길동");
+        map.put("addr","대구");
+        list.add(map);
+        map = new HashMap<String,String>();
+        map.put("name","김기자");
+        map.put("addr","제천");
+        list.add(map);
+        map = new HashMap<String,String>();
+        map.put("name","소나무");
+        map.put("addr","설");
+        list.add(map);
 
 
-
+        SimpleAdapter adapter = new SimpleAdapter(getApplicationContext(),list
+                ,android.R.layout.simple_list_item_2,new String[]{"name","addr"},new int[]{android.R.id.text1,android.R.id.text2});
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener((adapterView, view, i, l) -> {
+           Toast.makeText(getApplicationContext(),list.get(i).get("addr"),Toast.LENGTH_LONG).show();
+        });
 
         btn1.setOnClickListener(v->{
             Intent intent = new Intent(getApplicationContext(), InsertActivity.class);
@@ -60,16 +84,23 @@ public class MainActivity extends AppCompatActivity {
 
     btnASel.setOnClickListener(v->{
         String a="";
-        SQLiteDatabase db=dbHelper.getReadableDatabase();
+
+
+
         String sql = "select _id, name, age, mobile from emp order by _id desc ";
         Cursor cursor=db.rawQuery(sql, null);
-        while (cursor.moveToNext()){
+        while (cursor.moveToNext()) {
 
-            a+="id : "+cursor.getString(0)+" name : "+cursor.getString(1)+" age : "+cursor.getString(2)+" mobile : "+cursor.getString(3)+"\n";
-
+            a = " name : " + cursor.getString(1);
+          //  list.add(a);
         }
-        txtResult.setText(a);
+       // String[] data = new String[]{a};
+
+    //    lv.getAdapter().notifyDataSetChanged();
         db.close();
+
+
+
     });
 
 
